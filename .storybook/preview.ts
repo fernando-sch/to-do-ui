34321e4d4b15eaf88406ab241d25e79297c476c0
Fernando Schneider <fer.schneider.2000@gmail.com>
2024-04-23 19:43:28 -0300

    chore: setup storybook preview file

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,14 +1,9 @@
 import type { Preview } from "@storybook/react";
 
+import '../src/index.css';
+
 const preview: Preview = {
-  parameters: {
-    controls: {
-      matchers: {
-        color: /(background|color)$/i,
-        date: /Date$/i,
-      },
-    },
-  },
+  parameters: {},
 };
 
 export default preview;

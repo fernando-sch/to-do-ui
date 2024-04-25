b88d6d949c30ccc4250e5a9f091adb642779fb95
Fernando Schneider <fer.schneider.2000@gmail.com>
2024-04-25 19:01:19 -0300

    chore: add create task request on mock server

--- src/__tests__/mock-server.ts
+++ src/__tests__/mock-server.ts
@@ -7,17 +7,28 @@ export const startMockServer = () => {
     routes() {
       this.urlPrefix = envObject.apiBaseUrl;
 
+      this.post("/task", () => ({
+        data: {
+          id: "75cd2c7c-df04-4681-8644-147aaf82c1c9",
+          title: "My Task",
+          description: "Good description",
+          is_completed: false,
+        },
+      }));
+
       this.get("/tasks", () => ({
         data: [
           {
+            id: "75cd2c7c-df04-4681-8644-147aaf82c1c9",
             title: "My Task",
             description: "Good description",
-            "is_completed?": false,
+            is_completed: false,
           },
           {
+            id: "cc94fc3c-8f28-42f6-977e-a9e2d997e828",
             title: "My Second Task",
             description: null,
-            "is_completed?": true,
+            is_completed: true,
           },
         ],
       }));

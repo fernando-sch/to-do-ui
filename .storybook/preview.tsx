15f35758f31a6c76b1e887098d98a9c41828abf5
Fernando Schneider <fer.schneider.2000@gmail.com>
2024-05-02 11:46:09 -0300

    chore: update storybook preview file

---
+++ .storybook/preview.tsx
@@ -0,0 +1,20 @@
+import type { Preview } from "@storybook/react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import '../src/index.css';
+import { startMockServer } from "../src/__tests__/helpers"
+
+startMockServer();
+const client = new QueryClient();
+
+const preview: Preview = {
+  decorators: [
+    (Story) => (
+      <QueryClientProvider client={client}>
+        <Story />
+      </QueryClientProvider>
+    ),
+  ],
+  parameters: {},
+};
+
+export default preview;

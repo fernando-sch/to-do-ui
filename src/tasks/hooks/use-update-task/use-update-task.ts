07dabe02ee44bb9827e372a202e9d71977cdef18
Fernando Schneider <fer.schneider.2000@gmail.com>
2024-05-04 11:20:09 -0300

    feat: add update task hook

---
+++ src/tasks/hooks/use-update-task/use-update-task.ts
@@ -0,0 +1,25 @@
+import { axiosInstance } from "@/config";
+import { useQueryClient, useMutation } from "@tanstack/react-query";
+
+type TaskAttrs = {
+  id: string;
+  title?: string;
+  description?: string | null;
+  is_completed?: boolean;
+};
+
+export const useUpdateTask = () => {
+  const queryClient = useQueryClient();
+
+  const updateTaskMutation = useMutation({
+    mutationKey: ["updateTask"],
+    mutationFn: async (taskAttrs: TaskAttrs) => {
+      return await axiosInstance.patch(`/tasks/${taskAttrs.id}`, taskAttrs);
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["fetchTasks"] });
+    },
+  });
+
+  return { ...updateTaskMutation, updateTask: updateTaskMutation.mutateAsync };
+};

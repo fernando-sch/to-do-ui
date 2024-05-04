30cd109e5203b0b52d7f017f4059cca26ac12664
Fernando Schneider <fer.schneider.2000@gmail.com>
2024-05-04 17:31:31 -0300

    feat: add update task form

---
+++ src/tasks/components/update-form/update-form.styles.ts
@@ -0,0 +1,41 @@
+import styled from "styled-components";
+
+export const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  min-height: 180px;
+  width: fit-content;
+  padding: 10px;
+  background-color: #cec8c8;
+  border-radius: 5px;
+`;
+
+export const Label = styled.label`
+  color: #1c1c1c;
+  display: flex;
+  flex-direction: column;
+  min-height: 75px;
+  font-size: 1.2rem;
+`;
+
+export const Input = styled.input`
+  border: 1px solid #1c1c1c;
+  color: #1c1c1c;
+  background-color: #fff6f6;
+  border-radius: 5px;
+`;
+
+export const SubmitButton = styled.button`
+  color: #1c1c1c;
+  border: 1px solid #1c1c1c;
+  border-radius: 5px;
+  background-color: transparent;
+  padding: 0 5px;
+  font-size: 1.2rem;
+
+  &:hover {
+    cursor: pointer;
+  }
+`;
